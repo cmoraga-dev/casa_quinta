35888d4fff2bd581a0afbef923db6d618e7b06ae
Carlos Moraga <cmoragafarias@gmail.com>
2022-12-23 01:41:38 +0000

    test5-2

--- view/index.php
+++ view/index.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <script  src="../public/jquery-3.6.2.min.js"></script>
         <script src="../public/js/login-js.js"></script>
+        <script src="../public/js/jquery.Rut.js"></script>
         <script>
             // Auto formato para campo RUT
             $('#rutUser').Rut({
