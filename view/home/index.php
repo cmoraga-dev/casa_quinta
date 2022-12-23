9201def02f0f124c9db898427ec4dc13c953b147
Carlos Moraga <cmoragafarias@gmail.com>
2022-12-22 21:59:45 -0300

    Cambio de orden - llamado jquery

--- view/home/index.php
+++ view/home/index.php
@@ -6,8 +6,8 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <script  src="../../public/jquery-3.6.2.min.js"></script>
         <script src="../../public/js/home-index-js.js"></script> 
-        <script  src="../../public/jquery-3.6.2.min.js"></script>       
     </head>
     <body>
         <div class="header">
