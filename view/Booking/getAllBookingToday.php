1c82e318a7c888c017e34282b3061e2539f69385
eduardo munoz <emunoz.4f@gmail.com>
2022-12-28 20:59:25 -0300

    siguiendo realizando pruebas

--- view/Booking/getAllBookingToday.php
+++ view/Booking/getAllBookingToday.php
@@ -1,12 +1,10 @@
 <?php
-echo 'todo bien';
+
 require_once __DIR__.'/../../controller/Booking-controller.php';
 
-echo 'todo bien';
 
 $api = new Booking_controller();
 
-echo 'todo bien';
 // if($_SERVER["REQUEST_METHOD"] === "POST"){
 //     //$_POST = json_decode(file_get_contents("php://input"), true);
 //     $api->getAllBookingToday();
