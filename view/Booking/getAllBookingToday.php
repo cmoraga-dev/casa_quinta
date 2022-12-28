eadf2b37ebf0591fb519f20e7e440a4f5004157f
eduardo munoz <emunoz.4f@gmail.com>
2022-12-28 20:58:21 -0300

    pruebas por el error 500

--- view/Booking/getAllBookingToday.php
+++ view/Booking/getAllBookingToday.php
@@ -1,10 +1,14 @@
 <?php
+echo 'todo bien';
 require_once __DIR__.'/../../controller/Booking-controller.php';
+
+echo 'todo bien';
 
 $api = new Booking_controller();
 
-if($_SERVER["REQUEST_METHOD"] === "POST"){
-    //$_POST = json_decode(file_get_contents("php://input"), true);
-    $api->getAllBookingToday();
-}
+echo 'todo bien';
+// if($_SERVER["REQUEST_METHOD"] === "POST"){
+//     //$_POST = json_decode(file_get_contents("php://input"), true);
+//     $api->getAllBookingToday();
+// }
 ?>
