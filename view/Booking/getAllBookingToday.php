e9e5a68c065463c55eebe58374a25e98f62c46fc
eduardo munoz <emunoz.4f@gmail.com>
2022-12-28 21:06:03 -0300

    se responde un json pasando el post

--- view/Booking/getAllBookingToday.php
+++ view/Booking/getAllBookingToday.php
@@ -5,10 +5,14 @@ require_once __DIR__.'/../../controller/Booking-controller.php';
 
 $api = new Booking_controller();
 
-// if($_SERVER["REQUEST_METHOD"] === "POST"){
-//     //$_POST = json_decode(file_get_contents("php://input"), true);
-//     $api->getAllBookingToday();
-// }
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    //$_POST = json_decode(file_get_contents("php://input"), true);
+    //$api->getAllBookingToday();
 
-echo json_decode('se ha respondido bien');
+    echo json_encode(array(
+        'cod' => '202',
+        'def' => 'Reservas obtenidas con exito'
+    ));
+}
+
 ?>
