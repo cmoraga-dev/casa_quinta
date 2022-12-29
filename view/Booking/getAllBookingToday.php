728d799b939c626024949fcb7f389109699e9eed
eduardo munoz <emunoz.4f@gmail.com>
2022-12-28 21:08:12 -0300

    todo ha vuelto a lo normal

--- view/Booking/getAllBookingToday.php
+++ view/Booking/getAllBookingToday.php
@@ -7,12 +7,7 @@ $api = new Booking_controller();
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
     //$_POST = json_decode(file_get_contents("php://input"), true);
-    //$api->getAllBookingToday();
-
-    echo json_encode(array(
-        'cod' => '202',
-        'def' => 'Reservas obtenidas con exito'
-    ));
+    $api->getAllBookingToday();
 }
 
 ?>
