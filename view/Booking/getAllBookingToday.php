118d5464541bd2ff5e68b99b67866612e8ce4834
eduardo munoz <emunoz.4f@gmail.com>
2022-12-28 21:01:54 -0300

    respondemos un json

--- view/Booking/getAllBookingToday.php
+++ view/Booking/getAllBookingToday.php
@@ -9,4 +9,6 @@ $api = new Booking_controller();
 //     //$_POST = json_decode(file_get_contents("php://input"), true);
 //     $api->getAllBookingToday();
 // }
+
+echo json_decode('se ha respondido bien');
 ?>
