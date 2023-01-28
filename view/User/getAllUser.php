fafc801155c3a6e26174ef5934e038d323d4390d
Carlos Moraga <cmoragafarias@gmail.com>
2023-01-28 17:03:26 +0000

    users view test

--- view/User/getAllUser.php
+++ view/User/getAllUser.php
@@ -2,15 +2,14 @@
 require_once __DIR__.'/../../controller/User-controller.php';
 
 header('Content-type: application/json');
-echo json_encode( 'aaaaaaa' );
 
 
 $api = new User_controller();
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
     //$_POST = json_decode(file_get_contents("php://input"), true);
-    $api->getAlluser();
-    return 'Error!';
+    $data = $api->getAlluser();
+    echo json_encode($data);
 }
 
 ?>
