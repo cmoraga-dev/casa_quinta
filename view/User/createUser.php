21baead383ccfb12dba201eecc9af60013e4f476
carlos <carlos@syzlab.com>
2023-02-19 15:19:10 -0300

    add create user btn

--- view/User/createUser.php
+++ view/User/createUser.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../../controller/User-controller.php';
 $api = new User_controller();
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
-    $_POST = json_decode(file_get_contents("php://input"), true);
+    //$_POST = json_decode(file_get_contents("php://input"), true);
     $api->createUser($_POST['first_name'], $_POST['last_name'], $_POST['rut'], $_POST['email'] );
 }    
 ?>
