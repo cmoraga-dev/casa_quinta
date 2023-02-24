b57af3b68b9a451f2742c99624d7d7905dab75ec
carlos <carlos@syzlab.com>
2023-02-24 00:23:31 -0300

    accounts manager

--- view/Account/updateAccount.php
+++ view/Account/updateAccount.php
@@ -6,9 +6,9 @@ $api = new Account_controller();
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
 
-    if ($_POST['pass'] != ""){
-        $api->updatePassAccount($_POST['idAccount'],$_POST['pass']);
-    }
+    //if ($_POST['pass'] != ""){
+    //    $api->updatePassAccount($_POST['idAccount'],$_POST['pass']);
+    // }
     if ($_POST['name'] != ""){
         $api->updateNameAccount($_POST['idAccount'],$_POST['name']);
     }
