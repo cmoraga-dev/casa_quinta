ebf66b3f07b71941ed8996ce431235240e141a6b
escarlos <emunoz.4f@gmail.com>
2023-03-21 17:49:08 -0300

    se crean usuarios, ahora veremos si obtenemos su id

--- model/Account.php
+++ model/Account.php
@@ -38,9 +38,11 @@ include_once 'db.php';
 
                 $duplicateKey = $this->connect()->query("SELECT * FROM accounts WHERE user_name ='$name'");
                 $query = $this->connect()->prepare("INSERT INTO accounts (id_type_profile, user_name, pass) VALUES(2, '$name', '$pass')");                
-                if($duplicateKey->rowCount() < 1){                    
+                if($duplicateKey->rowCount() < 1){      
+                    //SELECT id FROM `accounts` ORDER BY `accounts`.`id` desc  limit 1                    
                     $query->execute();
-                    $last_id = $this->connect()->lastInsertId();
+                    $getLast_id = $this->connect()->query("SELECT id FROM `accounts` ORDER BY `accounts`.`id` desc  limit 1");
+                    $last_id = $getLast_id->$this->connect()->fetch_assoc();
                 }
                 return $last_id;
                 
