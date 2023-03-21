253d6fa653a40aa9a38eebdf58fcac07914abf3c
escarlos <emunoz.4f@gmail.com>
2023-03-21 16:59:26 -0300

    vemos que trata de enviar al server

--- controller/Account-controller.php
+++ controller/Account-controller.php
@@ -132,7 +132,7 @@ class Account_controller{
                 echo 'Se guardo Existosamente';
             }else{
                 echo json_encode(array('cod' => '500', 
-                                        'msj' => 'No se logro crear el usuario '+$res));
+                                        'msj' => 'No se logro crear el usuario '+$name + $pass + $id_type_profile));
             }
         } catch (\Throwable $th) {
             echo $th;
