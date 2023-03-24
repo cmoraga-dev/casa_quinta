c6766614ffacf358511cb9f697e27a0bb808b627
escarlos <emunoz.4f@gmail.com>
2023-03-24 14:16:43 -0300

    otro verificador de rut

--- public/js/accountsAdmin.js
+++ public/js/accountsAdmin.js
@@ -33,7 +33,7 @@ function createAccount(){
     const numero = rutSinFormato.slice(0, -1);
     const digitoVerificador = rutSinFormato.slice(-1);
 
-    console.log(Valida_Rut(rutSinFormato));
+    console.log(checkRut(rutSinFormato));
 
     // $.ajax({
     //     url: host+'/api/createAccount',
@@ -243,58 +243,50 @@ $(document).on('click','#cancel-create',function(event) {
 
  //------------------------ VALIDADOR DE RUT
 
-function Valida_Rut(Objeto) {
-    var tmpstr = "";
-    var intlargo = Objeto.toString().length;
-    if (intlargo.length > 0) {
-        crut = Objeto.value
-        largo = crut.length;
-        if (largo < 2) {
-            return false;
-        }
-        for (i = 0; i < crut.length; i++)
-            if (crut.charAt(i) != ' ' && crut.charAt(i) != '.' && crut.charAt(i) != '-') {
-                tmpstr = tmpstr + crut.charAt(i);
-            }
-        rut = tmpstr;
-        crut = tmpstr;
-        largo = crut.length;
-
-        if (largo > 2)
-            rut = crut.substring(0, largo - 1);
-        else
-            rut = crut.charAt(0);
-
-        dv = crut.charAt(largo - 1);
-
-        if (rut == null || dv == null)
-            return 0;
-
-        var dvr = '0';
-        suma = 0;
-        mul = 2;
-
-        for (i = rut.length - 1; i >= 0; i--) {
-            suma = suma + rut.charAt(i) * mul;
-            if (mul == 7)
-                mul = 2;
-            else
-                mul++;
-        }
-
-        res = suma % 11;
-        if (res == 1)
-            dvr = 'k';
-        else if (res == 0)
-            dvr = '0';
-        else {
-            dvi = 11 - res;
-            dvr = dvi + "";
-        }
-
-        if (dvr != dv.toLowerCase()) {
-            return false;
-        }
-        return true;
+ function checkRut(rut) {
+    // Despejar Puntos
+    var valor = rut.replace('.','');
+    // Despejar Guión
+    valor = valor.replace('-','');
+    
+    // Aislar Cuerpo y Dígito Verificador
+    cuerpo = valor.slice(0,-1);
+    dv = valor.slice(-1).toUpperCase();
+    
+    // Formatear RUN
+    rut.value = cuerpo + '-'+ dv
+    
+    // Si no cumple con el mínimo ej. (n.nnn.nnn)
+    if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+    
+    // Calcular Dígito Verificador
+    suma = 0;
+    multiplo = 2;
+    
+    // Para cada dígito del Cuerpo
+    for(i=1;i<=cuerpo.length;i++) {
+    
+        // Obtener su Producto con el Múltiplo Correspondiente
+        index = multiplo * valor.charAt(cuerpo.length - i);
+        
+        // Sumar al Contador General
+        suma = suma + index;
+        
+        // Consolidar Múltiplo dentro del rango [2,7]
+        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+  
     }
+    
+    // Calcular Dígito Verificador en base al Módulo 11
+    dvEsperado = 11 - (suma % 11);
+    
+    // Casos Especiales (0 y K)
+    dv = (dv == 'K')?10:dv;
+    dv = (dv == 0)?11:dv;
+    
+    // Validar que el Cuerpo coincide con su Dígito Verificador
+    if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+    
+    // Si todo sale bien, eliminar errores (decretar que es válido)
+    rut.setCustomValidity('');
 }
